5a6b42b3658f13d30563b34162a811494fb2a7a7
Horwerty <damiowat@gmail.com>
2022-01-22 05:20:53 +0100

    commit2

---
+++ script.js
@@ -0,0 +1,6 @@
+var menu = document.getElementById("menu");
+var UpperNav = document.getElementById("UpperNav");
+
+menu.addEventListener("click", function () {
+      UpperNav.classList.toggle("displayNav");
+});
