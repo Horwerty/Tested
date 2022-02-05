7daef8743de9112432bfc2643636b2e16ab8986d
Horwerty <damiowat@gmail.com>
2022-02-05 13:27:04 +0100

    First commit

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ var UpperNav = document.getElementById("UpperNav");
 menu.addEventListener("click", function () {
       UpperNav.classList.toggle("displayNav");
 });
+
+UpperNav.addEventListener("click", function () {
+      UpperNav.classList.remove("displayNav");
+});
